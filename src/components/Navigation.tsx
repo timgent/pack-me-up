cc6533502a6ff303e2e65e3376aa978fa4101efa
timgent <tim.gent@gmail.com>
2025-05-25 20:17:57 +0100

    Fix nav links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,7 @@ export const Navigation = () => {
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
                                 <Link
-                                    to="/"
+                                    to="/manage-questions"
                                     className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white"
                                 >
                                     Edit Questions
@@ -72,7 +72,7 @@ export const Navigation = () => {
             <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                     <Link
-                        to="/"
+                        to="/manage-questions"
                         className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700 hover:text-white"
                         onClick={() => setIsOpen(false)}
                     >
