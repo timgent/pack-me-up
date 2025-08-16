88c45c3788ea02dfcb592ce530fadbd18a09f8fa
timgent <tim.gent@gmail.com>
2025-08-16 21:07:27 +0200

    Rm unused imports

--- src/packing-lists/view-packing-list.tsx
+++ src/packing-lists/view-packing-list.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import PouchDB from 'pouchdb'
 import { useDebouncedCallback } from 'use-debounce'
