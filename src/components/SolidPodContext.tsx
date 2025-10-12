1adb5f0fbe0fd88841aec024910de3246347fcaa
timgent <tim.gent@gmail.com>
2025-10-12 15:24:13 +0100

    Fix solid pod context warnings

--- src/components/SolidPodContext.tsx
+++ src/components/SolidPodContext.tsx
@@ -1,29 +1,24 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
-// TODO: Don't default the IDP
-interface SolidPodDetails {
-    idp: "https://login.inrupt.com"
-}
-
 const SolidPodContext = createContext("moo");
 
 export function SolidPodProvider({ children }: { children: ReactNode }) {
-    const [solidPod, setSolidPod] = useState("moo")
+  const [solidPod, _setSolidPod] = useState("moo")
 
-    return (
-        < SolidPodContext.Provider value={solidPod}>
-            {children}
-        </SolidPodContext.Provider >
-    )
+  return (
+    < SolidPodContext.Provider value={solidPod}>
+      {children}
+    </SolidPodContext.Provider >
+  )
 
 }
 
 export function useSolidPod() {
-    const context = useContext(SolidPodContext);
+  const context = useContext(SolidPodContext);
 
-    if (context === undefined) {
-        throw new Error('useSolidPod must be used within a SolidPodProvider');
-    }
+  if (context === undefined) {
+    throw new Error('useSolidPod must be used within a SolidPodProvider');
+  }
 
-    return context;
+  return context;
 }
